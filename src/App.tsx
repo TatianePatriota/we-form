8828cd326f0d3a44a3b7f37e1c756055b92bbc18
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-05-29 19:31:10 -0300

    add register component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { Register } from "./components/Register";
 
 function App() {
-  return (
-    <Register
-      text="Sua entidade não se enquadra nos campos de atuação disponíveis?"
-      textLink="Registrar novo campo de atuação"
-    />
-  );
+  return <Register />;
 }
 
 export default App;
