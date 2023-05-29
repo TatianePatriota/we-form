0cb04c7086aab8dc4bc9bf0bead2d200a660bfdf
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-05-29 14:39:53 -0300

    add styles

--- src/components/CommonButton.tsx
+++ src/components/CommonButton.tsx
@@ -16,11 +16,15 @@ export function CommonButton({ text, color }: CommonButtontype) {
 
 export const StyledCommonButton = styled.a`
   text-decoration: none;
-  padding: 10px;
+  padding: 8px;
   background-color: ${({ theme }) => theme.colors.lightGreen};
   color: ${({ theme }) => theme.colors.white};
+  font-family: ${({ theme }) => theme.fonts.fontPoppins};
   display: block;
   text-align: center;
   border-radius: 12px;
   margin-bottom: 48px;
+  font-size: ${({ theme }) => theme.font.size.large}px;
+  line-height: 36px;
+  font-weight: 500;
 `;
