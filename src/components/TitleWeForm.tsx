bab0a28930e4b430d78a71262228486b32aa43df
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-05-29 19:31:29 -0300

    add title component

---
+++ src/components/TitleWeForm.tsx
@@ -0,0 +1,11 @@
+import { StyledTitleWeForm } from "./StyledTitleWeForm";
+
+export function TitleWeForm() {
+  return (
+    <StyledTitleWeForm>
+      <h1>
+        <span>We</span>Form
+      </h1>
+    </StyledTitleWeForm>
+  );
+}
