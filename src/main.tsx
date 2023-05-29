b23f8335bc506b883bda381ac9aed9160c43b2fc
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-05-29 19:30:37 -0300

    add Global Styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 
 import { theme } from "../Themes";
+
+const GlobalStyles = createGlobalStyle`
+    *, *::before, *::after {
+      box-sizing: border-box;
+    }
+`;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <App />
     </ThemeProvider>
   </React.StrictMode>
