5df5608ae40f0b54133c6097f0fff2ced38d1d83
Tatiane Patriota <tatianepatriota1@gmail.com>
2023-05-30 17:47:41 -0300

    add router

--- router.tsx
+++ router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom";
 import { RegisteredEntities } from "./src/routes/RegisteredEntities";
 import { Register } from "./src/routes/Register";
 import { RegisterEntity } from "./src/routes/RegisterEntity";
+import { RegisterField } from "./src/routes/RegisterField";
+import { RegistrationDone } from "./src/routes/RegistrationDone";
 
 export const router = createBrowserRouter([
   {
-    path: "/register",
+    path: "/",
     element: <Register />,
   },
   {
@@ -15,5 +17,13 @@ export const router = createBrowserRouter([
   {
     path: "/register-entity",
     element: <RegisterEntity />,
+  },
+  {
+    path: "/register-field",
+    element: <RegisterField />,
+  },
+  {
+    path: "/registration-done",
+    element: <RegistrationDone />,
   },
 ]);
